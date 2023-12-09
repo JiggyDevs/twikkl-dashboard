c68c1459c5d10853f8943b9810976ac7a15cde5f
AbdulAfeez Jimoh <jimohafeez738@gmail.com>
2023-12-09 20:59:26 +0100

    feat: dashboard home

--- src/app/(pages)/dashboard/layout.tsx
+++ src/app/(pages)/dashboard/layout.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="grid grid-cols-[0.3fr_0.7fr]">
+    <div className="grid grid-cols-[0.2fr_0.8fr]">
       <SideBar />
       <div className="bg-[#D7EBD9] flex flex-col">
         <NavBar />
