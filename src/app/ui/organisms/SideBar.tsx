7f2356cd6525e840f347305a660750cd4de80035
AbdulAfeez Jimoh <jimohafeez738@gmail.com>
2023-12-09 21:34:40 +0100

    feat: dashboard home

--- src/app/ui/organisms/SideBar.tsx
+++ src/app/ui/organisms/SideBar.tsx
@@ -5,7 +5,7 @@ import { CalendarTimeIcon, LogoutIcon } from "../../../../public/svg";
 
 const SideBar = () => {
   return (
-    <aside className="h-[100vh] md:h-screen flex flex-col pb-4 pt-2">
+    <aside className="h-[95vh] md:h-screen flex flex-col pb-4 pt-2">
       <div className="flex flex-col gap-y-4">
         <Link href="/dashboard" className="flex items-center">
           <Image
