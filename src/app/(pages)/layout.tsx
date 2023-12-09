c9a827924a33e7e0bf74e5d400fca8dd73b52b2b
AbdulAfeez Jimoh <jimohafeez738@gmail.com>
2023-12-09 20:51:53 +0100

    feat: dashboard home

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -47,7 +47,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="overflow-hidden">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <body className={`${axiforma.className}`}>{children}</body>
     </html>
   );
